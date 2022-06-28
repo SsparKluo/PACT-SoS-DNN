77d77f7e7161153b7ca7971711489cdb7af2798a
SsparKluo <ssparkluo@gmail.com>
2022-06-28 21:20:25 +0800

    new network and data structure

--- PACT_Dual_SoS_Segmentation/data_io.py
+++ PACT_Dual_SoS_Segmentation/data_io.py
@@ -9,8 +9,8 @@ import random
 
 
 class ImageDataGenerator(Sequence):
-    def __init__(self, path="../simulation_dual_SoS/", batch_size=8,
-                 shuffle=True, mode="train", data_augmentation="True", output_size=(192,256)) -> None:
+    def __init__(self, path="../simulation_dual_SoS/", batch_size=8, shuffle=True,
+                 mode="train", data_augmentation="True", output_size=(192, 256)) -> None:
         super().__init__()
         self.mode = mode
         if self.mode == 'train':
@@ -58,6 +58,64 @@ class ImageDataGenerator(Sequence):
             targets.append(np.array(gt))
         return np.expand_dims(
             np.array(inputs), axis=-1), np.expand_dims(np.array(targets), axis=-1)
+
+    def on_epoch_end(self):
+        if self.shuffle == True:
+            random.shuffle(self.indexes)
+
+
+class ImageDataGenerator2(Sequence):
+    def __init__(self, path="../simulation_dual_SoS/", batch_size=8, shuffle=True,
+                 mode="train", data_augmentation="True", output_size=(192, 256)) -> None:
+        super().__init__()
+        self.mode = mode
+        if self.mode == 'train':
+            self.paths = glob("{}[0-9]*".format(path))
+        elif self.mode == 'valid':
+            self.paths = glob("{}v_[0-9]*".format(path))
+        elif self.mode == 'test':
+            self.paths = glob("{}t_[0-9]*".format(path))
+
+        self.bs = batch_size
+        self.shuffle = shuffle
+        self.aug = data_augmentation
+        self.mag = 4 if self.aug else 2
+        self.len = len(self.paths) * self.mag
+        self.indexes = np.arange(self.len)
+        self.output_size = output_size
+        if self.shuffle:
+            random.shuffle(self.indexes)
+
+    def __len__(self):
+        return ceil(self.len / self.bs)
+
+    def __getitem__(self, idx):
+        batch_paths = self.indexes[idx * self.bs: (idx + 1) * self.bs]
+        inputs = []
+        targets = []
+        for i in batch_paths:
+            folderpath = self.paths[i // self.mag]
+            file_idx = i % self.mag // 2
+            # 0 for 128 channels image; 1 for 64 channels image
+
+            img = cv2.imread("{}/input_128.png".format(folderpath), 0) \
+                if file_idx == 0 else \
+                cv2.imread("{}/input_64.png".format(folderpath), 0)
+
+            img = cv2.flip(img, 1) if self.aug and i % 2 else img
+            # 0 for raw image; 1 for flipping
+            img = cv2.resize(img, self.output_size)
+            gt = loadmat("{}/GT.mat".format(folderpath))
+            gt = gt['GT']
+            gt = cv2.flip(gt, 1) if self.aug and i % 2 else gt
+            gt = (gt - gt.min()) / (gt.max() - gt.min())
+            gt = cv2.resize(gt, self.output_size)
+            gt = cv2.transpose(gt)
+            gt = [np.where(row==1)[0][0] for row in gt]
+
+            inputs.append(np.array(img))
+            targets.append(np.array(gt))
+        return np.expand_dims(np.array(inputs), axis=-1), np.array(targets)
 
     def on_epoch_end(self):
         if self.shuffle == True:
--- PACT_Dual_SoS_Segmentation/network.py
+++ PACT_Dual_SoS_Segmentation/network.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras import Input
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, Concatenate
+from tensorflow.keras.layers import Conv1D, UpSampling1D
 from tensorflow.keras.layers import UpSampling2D, Dropout, BatchNormalization, Cropping2D, LeakyReLU
 from tensorflow.keras.layers import Dense, Flatten, Reshape
 from tensorflow.keras import initializers, activations
@@ -83,7 +84,8 @@ def unet_with_denses(img_shape=(512, 384, 1),
         n = BatchNormalization()(n)
         n = LeakyReLU()(n) if acti == 'relu' else activations.sigmoid(n)
         n = Dropout(do)(n) if do else n
-        n = Dense(units=shape[0] * shape[1], kernel_initializer=initializers.HeNormal())(n)
+        n = Dense(units=shape[0] * shape[1],
+                  kernel_initializer=initializers.HeNormal())(n)
         return Reshape(target_shape=(shape[0], shape[1], 1))(n)
 
     def level_block(m, dim, depth, inc, acti, do, bn, mp, up, res, pd):
@@ -119,4 +121,87 @@ def unet_with_denses(img_shape=(512, 384, 1),
     output = level_block(input, start_ch, depth, inc_rate, activation,
                          dropout, batchnorm, maxpool, upconv, residual, padding)
     output = Conv2D(out_ch, 1, activation='sigmoid')(output)
+    return Model(inputs=input, outputs=output)
+
+
+def cnn_dense(img_shape=(256, 192, 1),
+              out_ch=1, start_ch=64, depth=4, inc_rate=2., activation='relu',
+              dropout=0.5, batchnorm=False, maxpool=True, upconv=True,
+              residual=False, padding='same'):
+
+    def conv_block(m, dim, acti, bn, res, do, pd='same'):
+        n = Conv2D(dim, 3, padding=pd, kernel_initializer=initializers.HeNormal())(m)
+        n = BatchNormalization()(n) if bn else n
+        n = LeakyReLU()(n) if acti == 'relu' else activations.sigmoid(n)
+        n = Dropout(do)(n) if do else n
+        n = Conv2D(dim, 3, padding=pd, kernel_initializer=initializers.HeNormal())(n)
+        n = BatchNormalization()(n) if bn else n
+        n = LeakyReLU()(n) if acti == 'relu' else activations.sigmoid(n)
+        return Concatenate()(
+            [Cropping2D(cropping=2 if padding == 'valid' else 0)(m),
+             n]) if res else n
+
+    def dense_link(m, acti, do, inner_dim, out_dim):
+        n = Flatten()(m)
+        n = Dense(units=inner_dim, kernel_initializer=initializers.HeNormal())(n)
+        n = BatchNormalization()(n)
+        n = LeakyReLU()(n) if acti == 'relu' else activations.sigmoid(n)
+        n = Dropout(do)(n) if do else n
+        n = Dense(units=out_dim, kernel_initializer=initializers.HeNormal())(n)
+        return Reshape((out_dim, 1))(n)
+
+    input = Input(shape=img_shape)
+
+    conv1 = conv_block(input, start_ch, activation,
+                       batchnorm, residual, dropout, padding)
+    maxpool1 = MaxPooling2D()(conv1)
+
+    conv2 = conv_block(maxpool1, start_ch * 2, activation,
+                       batchnorm, residual, dropout, padding)
+    maxpool2 = MaxPooling2D()(conv2)
+    dense2 = dense_link(maxpool2, activation, dropout, 192, 96)
+
+    conv3 = conv_block(maxpool2, start_ch * 4, activation,
+                       batchnorm, residual, dropout, padding)
+    maxpool3 = MaxPooling2D()(conv3)
+    dense3 = dense_link(maxpool3, activation, dropout, 96, 48)
+
+    conv4 = conv_block(maxpool3, start_ch * 8, activation,
+                       batchnorm, residual, dropout, padding)
+    maxpool4 = MaxPooling2D()(conv4)
+    dense4 = dense_link(maxpool4, activation, dropout, 48, 24)
+
+    up_dense4 = UpSampling1D(size=2)(dense4)
+    concat3 = Concatenate(axis=2)([up_dense4, dense3])
+    concat3 = Conv1D(4, 3, padding=padding,
+                     kernel_initializer=initializers.HeNormal())(concat3)
+    concat3 = BatchNormalization()(concat3)
+    concat3 = LeakyReLU()(concat3)
+    concat3 = Conv1D(2, 3, padding=padding,
+                     kernel_initializer=initializers.HeNormal())(concat3)
+    concat3 = BatchNormalization()(concat3)
+    concat3 = LeakyReLU()(concat3)
+
+    up_dense3 = UpSampling1D(size=2)(concat3)
+    concat2 = Concatenate(axis=2)([up_dense3, dense2])
+    concat2 = Conv1D(4, 3, padding=padding,
+                     kernel_initializer=initializers.HeNormal())(concat2)
+    concat2 = BatchNormalization()(concat2)
+    concat2 = LeakyReLU()(concat2)
+    concat2 = Conv1D(2, 3, padding=padding,
+                     kernel_initializer=initializers.HeNormal())(concat2)
+    concat2 = BatchNormalization()(concat2)
+    concat2 = LeakyReLU()(concat2)
+
+    up_dense2 = UpSampling1D(size=2)(concat2)
+    concat1 = Conv1D(4, 3, padding=padding,
+                     kernel_initializer=initializers.HeNormal())(up_dense2)
+    concat1 = BatchNormalization()(concat1)
+    concat1 = LeakyReLU()(concat1)
+    concat1 = Conv1D(2, 3, padding=padding,
+                     kernel_initializer=initializers.HeNormal())(concat1)
+    concat1 = BatchNormalization()(concat1)
+    concat1 = LeakyReLU()(concat1)
+
+    output = Conv1D(out_ch, 1, activation='tanh')(concat1)
     return Model(inputs=input, outputs=output)
