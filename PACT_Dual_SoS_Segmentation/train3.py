10def0c9a622b8e6c53e8fe4692dbf632c9f0b04
SsparKluo <ssparkluo@gmail.com>
2022-07-16 17:38:10 +0800

    sync

--- PACT_Dual_SoS_Segmentation/train3.py
+++ PACT_Dual_SoS_Segmentation/train3.py
@@ -9,8 +9,8 @@ import os
 from tensorflow.keras.models import load_model
 
 # Basic configuration
-bs = 8
-model_name = 'SoS_prediction_3'
+bs = 48
+model_name = 'SoS_prediction_5'
 saved_model = './saved_model/{}'.format(model_name)
 best_checkpoint = './saved_model/{}_best'.format(model_name)
 figure_path = './figure/{} - Model loss.png'.format(model_name)
@@ -30,26 +30,27 @@ encoder_model = load_model('./saved_model/unet_forFT')
 model = network.cnn_dense(img_shape=(256,192,1), batchnorm=True)
 
 for idx, layer in enumerate(model.layers):
-    if 'up' in layer.name:
+    if idx > 30:
         break
     if 'conv2d' not in layer.name:
         continue
     layer.set_weights(encoder_model.layers[idx].get_weights())
     layer.trainable = False
 '''
-model = load_model('./saved_model/SoS_prediction_2')
-for layer in model.layers:
-    layer.trainable = True
+model = load_model('./saved_model/SoS_prediction_4')
+#for layer in model.layers:
+#    layer.trainable = True
+
 model.summary()
 
 model.compile(loss=MeanSquaredError(),
               optimizer=optim.Adam(learning_rate=0.00005))
 
 reduce_lr = ReduceLROnPlateau(
-    verbose=1, factor=0.1, min_delta=0.0001, monitor='val_loss', patience=20,
+    verbose=1, factor=0.1, min_delta=0.0001, monitor='val_loss', patience=40,
     mode='auto', min_lr=0.00001)
 stop_condition = EarlyStopping(
-    monitor='val_loss', patience=41, min_delta=0.0001)
+    monitor='val_loss', patience=81, min_delta=0.0001)
 
 history = model.fit(trainset_loader, batch_size=bs, epochs=400,
                     validation_data=validset_loader, workers=16, max_queue_size=16,
